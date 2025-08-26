f5f7e1c6f58c1ddf98ebab979784f37d7fe5c722
Night <soren913@gmail.com>
2025-08-26 09:24:30 +0200

    Completed opgave 1 Medium

--- opgavefiler/js/medium.js
+++ opgavefiler/js/medium.js
@@ -1,7 +1,13 @@
 /* Opgave 1*/
 console.group("opgave 1");
+const data = {
+    egenskabItems: [
+        `Item1`, `Item2`, `Item3`, `Item4`, `Item5`
+    ]
+}
+data.egenskabItems.push(`NewString`)
 
-
+console.log(data.egenskabItems)
 
 console.groupEnd();
 
