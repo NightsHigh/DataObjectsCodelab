70a0d4dda060d2d5ada12bf17628c4e106aa5eea
Night <soren913@gmail.com>
2025-08-26 09:59:14 +0200

    Completed opgave 1 hard

--- opgavefiler/js/hard.js
+++ opgavefiler/js/hard.js
@@ -3,7 +3,21 @@
 
 
 console.group("opgave 1");
-
+const todoApp = {
+    work: 
+    {name: `Work toDo List`, 
+    item: [
+        {text: `Buy ingredients for crabby patties`, done: true},
+        {text: `Make Crabby patties`, done: false}
+    ]
+},
+    home:
+    {name: `Home toDo List`},
+    item: [
+        {text: `Walk gary`, done: true},
+        {text: `Feed gary`, done: false}
+    ]
+}
 console.groupEnd();
 /* opgave 2 */
 console.group("opgave 2");
